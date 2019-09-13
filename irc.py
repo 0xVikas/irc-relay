fa009b99a1bb3329d1af933b3e5b0d295abf0b55
0xVikas <vikasrvs@gmail.com>
2019-09-13 20:36:58 +0530

    Add comments

--- irc.py
+++ irc.py
@@ -1,6 +1,5 @@
 # https://github.com/0xVikas/irc_relay
 # Made for CRUx Round 2
-# Requires python 2.X
 
 import sys
 from twisted.internet import protocol, reactor
@@ -19,13 +18,17 @@ class ircServerProtocol(IRC, protocol.Protocol):
     def connectionMade(self):
         self.factory = protocol.ClientFactory()
         self.factory.protocol = ClientProtocol
+        # Configuring the server for the clientprotocol (Proxy's client)
+        # to send the data recieved from actual server
         self.factory.server = self
 
         if self.mainserver == None:
             get_site = b"Welcome!\nIRC Proxy - Made by Vikas\nEnter the IRC network address to connect :\nEx: /join irc.freenode.net\n\n"
+            # Writes the above message to client
             self.transport.write(get_site)
 
     def dataReceived(self, data):
+        # Redirect the data recieved from client to proxy's client
         if (self.client != None and self.mainserver != None):
             self.client.write(data)
         if self.mainserver == None and (b"JOIN" in data or b"join" in data):
@@ -34,6 +37,8 @@ class ircServerProtocol(IRC, protocol.Protocol):
             print(b"Server set to : " + self.mainserver)
             reactor.connectTCP(self.mainserver, 6667, self.factory)
 
+        # Appends all the data sent by client before
+        # connecting to a network to self.buffer
         if self.mainserver == None and (b"JOIN" not in data or b"join" not in data):
             print("Adding to buffer: " + str(data))
             self.buffer += data
@@ -44,12 +49,16 @@ class ircServerProtocol(IRC, protocol.Protocol):
 
 class ClientProtocol(protocol.Protocol):
     def connectionMade(self):
+        # Setting the Client for the server protocol
         self.factory.server.client = self
         print("writing to server : " + str(self.factory.server.buffer))
         self.write(self.factory.server.buffer)
+        # Clear the buffer once it is sent
         self.factory.server.buffer = ''
 
     def dataReceived(self, data):
+        # Redirect the data recieved from mainserver
+        # to the proxy's server
         self.factory.server.write(data)
 
     def write(self, data):
@@ -60,5 +69,6 @@ wordsRealm = service.InMemoryWordsRealm(None)
 myportal = portal.Portal(wordsRealm)
 factory = service.IRCFactory(wordsRealm, myportal)
 factory.protocol = ircServerProtocol
+# starts listening on the port 6667
 reactor.listenTCP(6667, factory)
 reactor.run()
